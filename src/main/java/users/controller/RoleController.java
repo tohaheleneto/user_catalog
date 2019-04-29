5c97cd40762e7d31390529667c19686b4fdc905a
tohaheleneto <anton_mamont@bk.ru>
2019-04-29 13:59:24 +0300

    after_deadline4 fixed role deliting

--- src/main/java/users/controller/RoleController.java
+++ src/main/java/users/controller/RoleController.java
@@ -8,12 +8,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import users.RoleRepository;
+import users.UserRepository;
 import users.entity.Role;
+import users.entity.User;
 
 @Controller
 public class RoleController {
     @Autowired
     private RoleRepository roleRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/addRole")
     public String addRolGet(Model model) {
@@ -42,6 +46,9 @@ public class RoleController {
     public String deleteRole(Model model,@RequestParam String RoleName) {
         Role role = roleRepository.findByName(RoleName);
         if (role != null) {
+            for (User user : userRepository.findAll()) {
+                user.getRoles().remove(role.getName());
+            }
             model.addAttribute("msg","Role deleted successfully");
             roleRepository.delete(role);
         }
